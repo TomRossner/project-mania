29fd947bdcca26a79685a060c9c79c04a5a06262
Tom Rossner <tomrossner2@gmail.com>
2023-05-11 01:20:42 +0300

    Updates

--- static/js/291.5619f5f4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_mania=self.webpackChunkproject_mania||[]).push([[291],{8291:function(n,u,e){e.r(u);var t=e(2791),a=e(7689),c=e(7800);u.default=function(){var n=(0,a.s0)(),u=(0,c.Z)().logout;return(0,t.useEffect)((function(){u(),n("/sign-in")}),[]),null}}}]);
-//# sourceMappingURL=291.5619f5f4.chunk.js.map
