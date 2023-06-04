f5592fe46ae3124a4de5dd42b6df5493119c448f
Tom Rossner <tomrossner2@gmail.com>
2023-06-04 23:22:08 +0300

    Fix logo click

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {AiFillProject} from "react-icons/ai";
 import IconContainer from './IconContainer';
+import useProject from '../../hooks/useProject';
 
 const Logo = () => {
+  const {closeMenu} = useProject();
   const navigate = useNavigate();
-  const handleLogoClick = () => navigate("/");
+
+  const handleLogoClick = () => {
+    closeMenu();
+    navigate("/");
+  }
 
   return (
     <div className='logo-container' onClick={handleLogoClick}>
